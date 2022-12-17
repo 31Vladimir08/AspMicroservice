7bba435720371cacf8b592e8d4694494967e809c
Vladimir <vladimir86udm@gmail.com>
2022-12-17 13:02:28 +0400

    Added MediatR

---
+++ Services/Ordering/Ordering.Application/Contracts/Persistence/IOrderRepository.cs
@@ -0,0 +1,9 @@
+﻿using Ordering.Domain.Entitites;
+
+namespace Ordering.Application.Contracts.Persistence
+{
+    public interface IOrderRepository : IAsyncRepository<Order>
+    {
+        Task<IEnumerable<Order>> GetOrdersByUserName(string userName);
+    }
+}
